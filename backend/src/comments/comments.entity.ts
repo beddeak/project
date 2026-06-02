055811d2729e43c69c2b74753172b61d4afa0932
beddeak <bedding1083@gmail.com>
2026-06-02 15:16:59 +0900

    comments db 만드는중

--- backend/src/comments/comments.entity.ts
+++ backend/src/comments/comments.entity.ts
@@ -1,1 +1,19 @@
 import { Column,Entity,PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('comments')
+export class CommentsEntity{
+    @PrimaryGeneratedColumn()
+    id!:number
+
+    @Column()
+    postId!:number
+
+    @Column()
+    content!:string
+    
+    @Column()
+    authorId!:number
+
+    @Column()
+    authorName!:string
+}
