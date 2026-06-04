dbb23c8f5f69168b9caa52ac89eda556ffaadc27
beddeak <bedding1083@gmail.com>
2026-06-05 08:54:47 +0900

    ?

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './users.entity';
+import { Repository } from 'typeorm';
+import { create } from 'domain';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor (
+        @InjectRepository(UserEntity)
+        private readonly UserServiceRepository: Repository<UserEntity>
+    ) {}
+    async 
+}
